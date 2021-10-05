6cdba9d6849570ebb68fad004bdea538d9ebd431
Santhusha-bit <74499826+Santhusha-bit@users.noreply.github.com>
2021-10-05 14:33:50 +0530

    Range slider and Output text
    Range slider and Output text were done

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+   amount = document.getElementById("principal").value;
+   rate = document.getElementById('textInput').value;
+   years = document.getElementById("years").value; 
+   result = (amount*(rate/100))*years;
+
+   document.getElementById("out1").innerHTML = 'If you deposit '+amount;
+   document.getElementById("out2").innerHTML ='at an interest rate of '+rate+'%';
+   document.getElementById("out3").innerHTML =' You will receive an amount of '+result;
+   document.getElementById("out4").innerHTML =' in the year '+ (parseInt(years)+2020)     
+}
+
+function updateTextInput(val) {
+   document.getElementById('textInput').value=val; 
 }
         
